96ab3e39d76bf47252ff057ed6b20c703bd77a6b
Albx68 <albertyumnam3D@gmail.com>
2024-05-06 23:41:22 +0530

    added some accessibility features

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -23,7 +23,10 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
 
                 }
-            }
+            }  .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
+
             Circle().strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
@@ -31,7 +34,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                                 Image(systemName: "forward.fill")
                             }
-                      }
+            }.accessibilityLabel("Next speaker")
         }.padding()
 
        
