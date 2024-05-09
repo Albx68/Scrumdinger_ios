b4ab07403e85e6c2f6b629cca43ee0a4e8ab1af3
Albx68 <albertyumnam3D@gmail.com>
2024-05-09 16:05:21 +0530

    apply theme to cards

--- Scrumdinger/Models/ScrumsView.swift
+++ Scrumdinger/Models/ScrumsView.swift
@@ -5,7 +5,7 @@ struct ScrumsView: View {
     let scrums:[DailyScrum]
     var body: some View {
         List(scrums,id:\.title){ scrum in
-            CardView(scrum: scrum)
+            CardView(scrum: scrum).listRowBackground(scrum.theme.mainColor)
         }
     }
 }
